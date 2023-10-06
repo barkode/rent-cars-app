bc423748a3084f785528f0d090220cdd1947c92e
barkode <konstantinks@gmail.com>
2023-10-06 23:34:10 +0100

    Add pages

---
+++ src/pages/CarCatalog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CarCatalog = () => {
+  return (
+    <>
+      <h1>THIS PAGE IS CAR CATALOG</h1>
+    </>
+  );
+};
+
+export default CarCatalog;
---
+++ src/pages/Favorite.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Favorite = () => {
+  return (
+    <>
+      <h1>THIS IS FAVORITE PAGE</h1>
+    </>
+  );
+};
+export default Favorite;
