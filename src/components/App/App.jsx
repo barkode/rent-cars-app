bf994d04529b4b6c21171b5a16d2def5d98b9acd
barkode <konstantinks@gmail.com>
2023-10-07 00:34:19 +0100

    Add SharedLayout Component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
+import SharedLayout from 'components/SharedLayout/SharedLayout';
 import About from 'pages/AboutPage';
 import CarCatalog from 'pages/CarCatalog';
 import Favorite from 'pages/Favorite';
 import NotFound from 'pages/NotFound';
 
+
 export const App = () => {
   return (
     <>
-      <nav>
-        <NavLink to="/">HOME</NavLink>
-        <NavLink to="/catalog">CAR CATALOG</NavLink>
-        <NavLink to="/favorite">FAVORITE</NavLink>
-      </nav>
       <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="/catalog" element={<CarCatalog />} />
-        <Route path="/favorite" element={<Favorite />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<About />} />
+          <Route path="catalog" element={<CarCatalog />} />
+          <Route path="favorite" element={<Favorite />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </>
   );
---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+
+const SharedLayout = () => {
+  return (
+    <>
+      <p>SharedLayout</p>
+      <nav>
+        <NavLink to="/">HOME</NavLink>
+        <NavLink to="/catalog">CAR CATALOG</NavLink>
+        <NavLink to="/favorite">FAVORITE</NavLink>
+      </nav>
+      {Outlet}
+    </>
+  );
+};
+
+export default SharedLayout;
