8cd246f5145d81f7f0ff306578aceeb9e4d625f2
barkode <konstantinks@gmail.com>
2023-10-07 16:30:32 +0100

    Add import from index.js file

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export * from 'components/App/App';
+export * from 'components/CarItem/CarItem';
+export * from 'components/CarList/CarList';
+export * from 'components/CloseButton/CloseButton';
+export * from 'components/ErrorMessage/ErrorMessage';
+export * from 'components/SharedLayout/SharedLayout';
+export * from 'components/Wrapper/Wrapper';
