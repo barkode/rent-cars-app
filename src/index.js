67d5852e47d7611c3190bf6ef68998626ba1e79d
barkode <konstantinks@gmail.com>
2023-10-08 01:34:39 +0100

    Add package modern-normalizer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App/App';
+import 'modern-normalize';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
