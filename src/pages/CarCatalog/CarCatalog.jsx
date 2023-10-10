70e567644a83336fd1c4c265b361c0537372eb54
barkode <konstantinks@gmail.com>
2023-10-10 13:51:21 +0100

    Add markup to Car Catalog page

--- src/pages/CarCatalog/CarCatalog.jsx
+++ src/pages/CarCatalog/CarCatalog.jsx
@@ -1,17 +1,55 @@
-import React from 'react';
-import requestApi from '../../utils/requestApi';
+import React, { useEffect, useState } from 'react';
+// import requestApi from '../../utils/requestApi';
 import { Helmet } from 'react-helmet-async';
+import CarList from 'components/CarList/CarList';
+import axios from 'axios';
 
 const CarCatalog = () => {
-  console.log(requestApi());
+  const [carItems, setCarItems] = useState(null);
+  const [page, setPage] = useState(1);
+  const [isBtnShown, setIsBtnShown] = useState(true);
+  // const [showModal, setShowModal] = useState(false);
+  const [isError, setIsError] = useState(null);
+
+  useEffect(() => {
+    const getCars = async () => {
+      try {
+        const res = await axios.get(
+          'https://65202ee6906e276284c42681.mockapi.io/advert'
+        );
+        setCarItems(res.data);
+        console.log(res.data);
+      } catch (error) {
+        setIsError(error.request);
+      }
+    };
+    getCars();
+  }, []);
+
+  // const toggleModal = () => {
+  //   setShowModal(prev => (prev = !prev));
+  // };
+
+  // const getCars = () => {
+  //   try {
+  //     const res = requestApi();
+  //     setCarItems(res.data);
+  //     console.log(carItems);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   return (
-    <>
+    <main>
       <Helmet>
         <title>Rent cars catalog</title>
       </Helmet>
-      <h1>THIS PAGE IS CAR CATALOG</h1>
-    </>
+      <main>
+        {carItems ? <CarList carItems={carItems} /> : <div>LOADING.....</div>}
+        <button type="button">Load more</button>
+      </main>
+    </main>
   );
 };
 
